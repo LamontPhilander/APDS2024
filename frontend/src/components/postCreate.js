386e5288a3b4a51cbe31f211ab65bb3aab8493b7
Lamont <106991652+LamontPhilander@users.noreply.github.com>
2024-10-17 09:20:09 +0200

    Delete frontend directory

--- frontend/src/components/postCreate.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const postCreate = () => {
-  return (
-    <div>postCreate</div>
-  )
-}
-
-export default postCreate
--- frontend/src/components/postEdit.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const postEdit = () => {
-  return (
-    <div>postEdit</div>
-  )
-}
-
-export default postEdit
